94f32476938981880e42f005d26876cef196c640
cypsun <cypyyy2012>
2013-03-01 12:06:53 +0800

    //second modify

--- src/com/lkk/mobile/Main.java
+++ src/com/lkk/mobile/Main.java
@@ -46,9 +46,9 @@ import com.lkk.mobile.tools.NetUtil;
 
 public class Main extends Activity {
 
-	private static final int PHOTO_REQUEST_TAKEPHOTO = 1;// 拍照
-	private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
-	private static final int PHOTO_REQUEST_CUT = 3;// 结果
+	private static final int PHOTO_REQUEST_TAKEPHOTO = 1;// 鎷嶇収
+	private static final int PHOTO_REQUEST_GALLERY = 2;// 浠庣浉鍐屼腑閫夋嫨
+	private static final int PHOTO_REQUEST_CUT = 3;// 缁撴灉
 	private static final int PHOTO_REQUEST_TAKEPHOTO_FORADD = 4;
 	private static final int PHOTO_REQUEST_GALLERY_FORADD = 5;
 	private File tempFile ;
@@ -56,9 +56,9 @@ public class Main extends Activity {
 	private static final String IMAGE_FILE_LOCATION = "file:///"+NetUtil.IMG_UPLOAD_ADDR+"temp.jpg";
 	private Uri tempImageUri;
 	private Uri tempContentImageUri;
-	private static final String[] m = { "不促销", "促销" }; // spinner 填充数据
+	private static final String[] m = { "涓嶄績閿�, "淇冮攢" }; // spinner 濉厖鏁版嵁
 	private ArrayAdapter<String> adapter;
-	// 控件
+	// 鎺т欢
 	private EditText e_title;
 	private EditText e_contentShort;
 	private EditText e_content;
@@ -71,20 +71,20 @@ public class Main extends Activity {
 	private ImageView contentpic_show;
 	private ImageView image_show;
 
-	// 变量
+	// 鍙橀噺
 	private String title;
 	private String contentShort;
 	private String content;
 	private String promotion;
 	private String pricePro;
 	private String price;
-	// 图片功能 consts
+	// 鍥剧墖鍔熻兘 consts
 
-	private String fileName; // 图片名称
+	private String fileName; // 鍥剧墖鍚嶇О
 	private File photo_file_addcontent = null;
-	private File photo_file = null; // 图片文件
-	private ProgressDialog delLoadingDialog = null; // 进度条···
-//	private Uri imageFilePath; // 照片路径
+	private File photo_file = null; // 鍥剧墖鏂囦欢
+	private ProgressDialog delLoadingDialog = null; // 杩涘害鏉÷仿仿�
+//	private Uri imageFilePath; // 鐓х墖璺緞
 	private Date today;
 	private byte[] image_Content;
 //	private Bitmap myBitmap;
@@ -109,19 +109,19 @@ public class Main extends Activity {
 		Log.e("tempImageUri", String.valueOf(tempImageUri));
 		Log.e("tempContentImageUri", String.valueOf(tempContentImageUri));
 		
-		e_title = (EditText) findViewById(R.id.textView1_2_title); // 标题
-		e_contentShort = (EditText) findViewById(R.id.textView2_2_contentShort);// 广告简介
-		e_content = (EditText) findViewById(R.id.textView3_2_content); // 广告内容
-		s_promotion = (Spinner) findViewById(R.id.spiView4_2_promotion); // 是否促销
-		e_pricePro = (EditText) findViewById(R.id.textView5_2_pricePro); // 促销价格
-		e_pricePro.setVisibility(View.INVISIBLE); // 默认不可见
-		e_price = (EditText) findViewById(R.id.textView6_2_price); // 原价格
-		b_contentpic = (Button) findViewById(R.id.content_pic); // 广告内容图片 button
-		b_file = (Button) findViewById(R.id.button_file); // 上传图片
-		submit = (Button) findViewById(R.id.buttom_submit); // 提交
+		e_title = (EditText) findViewById(R.id.textView1_2_title); // 鏍囬
+		e_contentShort = (EditText) findViewById(R.id.textView2_2_contentShort);// 骞垮憡绠�粙
+		e_content = (EditText) findViewById(R.id.textView3_2_content); // 骞垮憡鍐呭
+		s_promotion = (Spinner) findViewById(R.id.spiView4_2_promotion); // 鏄惁淇冮攢
+		e_pricePro = (EditText) findViewById(R.id.textView5_2_pricePro); // 淇冮攢浠锋牸
+		e_pricePro.setVisibility(View.INVISIBLE); // 榛樿涓嶅彲瑙�
+		e_price = (EditText) findViewById(R.id.textView6_2_price); // 鍘熶环鏍�
+		b_contentpic = (Button) findViewById(R.id.content_pic); // 骞垮憡鍐呭鍥剧墖 button
+		b_file = (Button) findViewById(R.id.button_file); // 涓婁紶鍥剧墖
+		submit = (Button) findViewById(R.id.buttom_submit); // 鎻愪氦
 		image_show = (ImageView) findViewById(R.id.Pic_show);
-		contentpic_show = (ImageView) findViewById(R.id.contentPic_show); // 广告内容图片展示
-		// 处理spinner
+		contentpic_show = (ImageView) findViewById(R.id.contentPic_show); // 骞垮憡鍐呭鍥剧墖灞曠ず
+		// 澶勭悊spinner
 		adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, m);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		s_promotion.setAdapter(adapter);
@@ -134,8 +134,8 @@ public class Main extends Activity {
 			public boolean onLongClick(View v) {
 				// TODO Auto-generated method stub
 
-				final String form[] = { "模板一", "模板二", "模板三" };
-				new AlertDialog.Builder(Main.this).setTitle("选择广告模板").setItems(
+				final String form[] = { "妯℃澘涓�, "妯℃澘浜�, "妯℃澘涓� };
+				new AlertDialog.Builder(Main.this).setTitle("閫夋嫨骞垮憡妯℃澘").setItems(
 						form, new DialogInterface.OnClickListener() {
 
 							@Override
@@ -163,22 +163,22 @@ public class Main extends Activity {
 				pricePro = e_pricePro.getText().toString();
 				price = e_price.getText().toString();
 				if (title == null || "".equals(title)) {
-					Toast.makeText(Main.this, "标题不能为空", Toast.LENGTH_SHORT)
+					Toast.makeText(Main.this, "鏍囬涓嶈兘涓虹┖", Toast.LENGTH_SHORT)
 							.show();
 					e_title.setFocusable(true);
 					e_title.setFocusableInTouchMode(true);
 					e_title.requestFocus();
 					e_title.requestFocusFromTouch();
 				} else {
-					// 还差一个图片路径
-					// 多线程 提交广告
+					// 杩樺樊涓�釜鍥剧墖璺緞
+					// 澶氱嚎绋�鎻愪氦骞垮憡
 					URL url = null;
 					try {
 						url = new URL(NetUtil.SERVER_IP + "AdInput!addAd");
 					} catch (MalformedURLException e) {
 						e.printStackTrace();
-					} // 网络URL
-					new PostAdTask().execute(url); // 启动线程
+					} // 缃戠粶URL
+					new PostAdTask().execute(url); // 鍚姩绾跨▼
 
 				}
 
@@ -193,8 +193,8 @@ public class Main extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				new AlertDialog.Builder(Main.this).setTitle("选择图片方式")
-						.setMessage("选择").setPositiveButton("相机拍照",
+				new AlertDialog.Builder(Main.this).setTitle("閫夋嫨鍥剧墖鏂瑰紡")
+						.setMessage("閫夋嫨").setPositiveButton("鐩告満鎷嶇収",
 								new DialogInterface.OnClickListener() {
 									public void onClick(DialogInterface dialog,
 											int whichButton) {
@@ -211,10 +211,10 @@ public class Main extends Activity {
 
 										startActivityForResult(
 												take_photo_intent,
-												PHOTO_REQUEST_TAKEPHOTO_FORADD); // 使用startActivityForReaule
+												PHOTO_REQUEST_TAKEPHOTO_FORADD); // 浣跨敤startActivityForReaule
 																					
 									}
-								}).setNegativeButton("手机相册",
+								}).setNegativeButton("鎵嬫満鐩稿唽",
 								new DialogInterface.OnClickListener() {
 									public void onClick(DialogInterface dialog,
 											int whichButton) {
@@ -234,21 +234,21 @@ public class Main extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 
-				new AlertDialog.Builder(Main.this).setTitle("选择图片方式")
-						.setMessage("选择").setPositiveButton("相机拍照",
+				new AlertDialog.Builder(Main.this).setTitle("閫夋嫨鍥剧墖鏂瑰紡")
+						.setMessage("閫夋嫨").setPositiveButton("鐩告満鎷嶇収",
 								new DialogInterface.OnClickListener() {
 									public void onClick(DialogInterface dialog,
 											int whichButton) {
 										Intent take_photo_intent = new Intent(
 												MediaStore.ACTION_IMAGE_CAPTURE);
-										// 指定调用相机拍照后照片的储存路径
+										// 鎸囧畾璋冪敤鐩告満鎷嶇収鍚庣収鐗囩殑鍌ㄥ瓨璺緞
 										take_photo_intent.putExtra(
 												MediaStore.EXTRA_OUTPUT, tempImageUri);
 										startActivityForResult(
 												take_photo_intent,
 												PHOTO_REQUEST_TAKEPHOTO);
 									}
-								}).setNegativeButton("手机相册",
+								}).setNegativeButton("鎵嬫満鐩稿唽",
 								new DialogInterface.OnClickListener() {
 									public void onClick(DialogInterface dialog,
 											int whichButton) {
@@ -268,14 +268,14 @@ public class Main extends Activity {
 	}
 
 	/*
-	 * 使用数组形式操作 spinner
+	 * 浣跨敤鏁扮粍褰㈠紡鎿嶄綔 spinner
 	 */
 	class SpinnerSelectedListener implements OnItemSelectedListener {
 
 		public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
 				long arg3) {
-			promotion = m[arg2]; // 促销
-			if (m[arg2].equals("促销")) {
+			promotion = m[arg2]; // 淇冮攢
+			if (m[arg2].equals("淇冮攢")) {
 				promotion = "1";
 				e_pricePro.setVisibility(View.VISIBLE);
 			} else { // no promot
@@ -295,18 +295,18 @@ public class Main extends Activity {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
 			AlertDialog.Builder alertbBuilder = new AlertDialog.Builder(this);
-			alertbBuilder.setTitle("系统信息").setMessage("确定要退出程序？")
-					.setPositiveButton("确定",
+			alertbBuilder.setTitle("绯荤粺淇℃伅").setMessage("纭畾瑕侀�鍑虹▼搴忥紵")
+					.setPositiveButton("纭畾",
 							new DialogInterface.OnClickListener() {
 
 								@Override
 								public void onClick(DialogInterface dialog,
 										int which) {
-									// 结束这个Activity
+									// 缁撴潫杩欎釜Activity
 									int nPid = android.os.Process.myPid();
 									android.os.Process.killProcess(nPid);
 								}
-							}).setNegativeButton("取消",
+							}).setNegativeButton("鍙栨秷",
 							new DialogInterface.OnClickListener() {
 
 								@Override
@@ -356,14 +356,14 @@ public class Main extends Activity {
 //				myBitmap = getPicFromBytes(image_Content, null);
 				Bitmap bitmap = decodeUriAsBitmap(tempContentImageUri);
 				Log.e("bitmap",String.valueOf(bitmap));
-				//contentpic_show.setImageBitmap(myBitmap); // 预览功能
+				//contentpic_show.setImageBitmap(myBitmap); // 棰勮鍔熻兘
 				contentpic_show.setImageBitmap(bitmap);
 				BufferedOutputStream bos = new BufferedOutputStream(
 						new FileOutputStream(String.format(
 								NetUtil.IMG_UPLOAD_ADDR + fileName + ".jpg",
 								System.currentTimeMillis())));
 				photo_file_addcontent = new File(NetUtil.IMG_UPLOAD_ADDR
-						+ fileName + ".jpg"); // 上传的文件路径
+						+ fileName + ".jpg"); // 涓婁紶鐨勬枃浠惰矾寰�
 				Log.e("photo_file_addcontent",photo_file_addcontent.toString());
 				bitmap.compress(Bitmap.CompressFormat.JPEG, 60, bos);
 				bos.flush();
@@ -395,7 +395,7 @@ public class Main extends Activity {
 
 	}
 
-	// 将进行剪裁后的图片显示到UI界面上,并存储
+	// 灏嗚繘琛屽壀瑁佸悗鐨勫浘鐗囨樉绀哄埌UI鐣岄潰涓�骞跺瓨鍌�
 	private void setPicToView(Intent picdata) {
 		Bundle bundle = picdata.getExtras();
 		if (bundle != null) {
@@ -419,7 +419,7 @@ public class Main extends Activity {
 						bos.flush();
 						bos.close();
 						photo_file = new File(NetUtil.IMG_UPLOAD_ADDR
-								+ fileName + ".jpg"); // 上传的文件路径
+								+ fileName + ".jpg"); // 涓婁紶鐨勬枃浠惰矾寰�
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -430,7 +430,7 @@ public class Main extends Activity {
 		}
 	}
 
-	// 裁剪照片
+	// 瑁佸壀鐓х墖
 	void startPhotoZoom(Uri uri, int size) {
 		Log.e("zoom", "begin");
 		Log.e("uri", String.valueOf(uri));
@@ -460,7 +460,7 @@ public class Main extends Activity {
 		return bitmap;
 	}
 	
-	// 读取图片
+	// 璇诲彇鍥剧墖
 	public static byte[] readStream(InputStream inStream) throws Exception {
 		byte[] buffer = new byte[1024];
 		int len = -1;
@@ -475,7 +475,7 @@ public class Main extends Activity {
 
 	}
 
-	// 转换成bitmap
+	// 杞崲鎴恇itmap
 	public static Bitmap getPicFromBytes(byte[] bytes,
 			BitmapFactory.Options opts) {
 		if (bytes != null){
@@ -491,7 +491,7 @@ public class Main extends Activity {
 
 	}
 
-	// 使用系统当前日期加以调整作为照片的名称
+	// 浣跨敤绯荤粺褰撳墠鏃ユ湡鍔犱互璋冩暣浣滀负鐓х墖鐨勫悕绉�
 	private String getPhotoFileName() {
 		Date date = new Date(System.currentTimeMillis());
 		SimpleDateFormat dateFormat = new SimpleDateFormat(
@@ -499,7 +499,7 @@ public class Main extends Activity {
 		return  dateFormat.format(date) + ".jpg";
 	}
 
-	// 网络访问线程
+	// 缃戠粶璁块棶绾跨▼
 	private class PostAdTask extends AsyncTask<URL, Integer, String> {
 
 		ProgressDialog progressDialog;
@@ -509,13 +509,13 @@ public class Main extends Activity {
 			// TODO Auto-generated method stub
 			Log.e("M2", "begin");
 			super.onPreExecute();
-			progressDialog = ProgressDialog.show(Main.this, "提交中", "请稍后····");
+			progressDialog = ProgressDialog.show(Main.this, "鎻愪氦涓�, "璇风◢鍚幝仿仿仿�);
 		}
 
 		protected String doInBackground(URL... urls) {
 			Log.e("M22", "begin");
 
-			String status = null; // 是否提交成功
+			String status = null; // 鏄惁鎻愪氦鎴愬姛
 
 			Map<String, String> addForm = new HashMap<String, String>();
 
@@ -575,20 +575,20 @@ public class Main extends Activity {
 	}
 
 	/**
-	 * 录入完成后，显示处理结果
+	 * 褰曞叆瀹屾垚鍚庯紝鏄剧ず澶勭悊缁撴灉
 	 * 
 	 * @param result
 	 */
 	private void showResultDialog(String result) {
-		// 创建提示
-		new AlertDialog.Builder(Main.this).setTitle("发布广告结果")
-				.setMessage(result).setPositiveButton("确定",
+		// 鍒涘缓鎻愮ず
+		new AlertDialog.Builder(Main.this).setTitle("鍙戝竷骞垮憡缁撴灉")
+				.setMessage(result).setPositiveButton("纭畾",
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int whichButton) {
 
 							}
-						}).setNegativeButton("继续录入",
+						}).setNegativeButton("缁х画褰曞叆",
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int whichButton) {
@@ -597,7 +597,7 @@ public class Main extends Activity {
 								e_content.setText("");
 								e_price.setText("");
 								e_pricePro.setText("");
-								// 设置焦点
+								// 璁剧疆鐒︾偣
 								e_title.setFocusable(true);
 								e_title.setFocusableInTouchMode(true);
 								e_title.requestFocus();
@@ -612,5 +612,8 @@ public class Main extends Activity {
 		// test GIT
 	}
 	
-	
+	public void test2(){
+	    //the second mondify	
+	}
+	}
 }
